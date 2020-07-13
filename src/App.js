2c0c096f74d7cef6d840f06528850d2a52788650
nhut <nguyendongnhutqn1996@gmail.com>
2020-07-14 00:42:51 +0700

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Đi chơi" },
+    { id: 2, title: "Đi ăn" },
+    { id: 3, title: "Đi ngủ" },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+
+    newTodoList.splice(index, 1);
+
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to the game</h1>
-      <ColorBox />
+      <h1>React Hooks - TodoList</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
